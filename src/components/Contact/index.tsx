a3c137d77bb605ba81c28b2e74392f2bf5216162
Adam Moore <adam@trainerroad.com>
2025-02-19 20:17:51 -0800

    updates

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -56,17 +56,11 @@ const Contact = () => {
   };
 
   return (
-    <section
-      id="contact"
-      className="overflow-hidden bg-primary py-16 md:py-20 lg:py-28"
-    >
+    <section id="contact" className="overflow-hidden  py-16 md:py-20 lg:py-28">
       <div className="container">
         <div className="-mx-4 flex flex-wrap">
           <div className="mx-auto w-full px-4 lg:w-7/12 xl:w-8/12">
-            <div
-              className="mb-12 rounded-sm bg-white px-8 py-11 shadow-three dark:bg-gray-dark sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]"
-              data-wow-delay=".15s"
-            >
+            <div className="rounded-sm" data-wow-delay=".15s">
               <h2 className="mb-3 text-2xl font-bold text-black dark:text-white sm:text-3xl lg:text-2xl xl:text-3xl">
                 Get in Touch
               </h2>
